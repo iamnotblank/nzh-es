a60f3c11adcd6838c9ec6bbb206f9f889d90e799
沈赞新 <shenzanxin@transtella.com>
2026-01-19 09:45:57 +0800

    refactor(dist): 重构nzh.cn.js代码结构
    移除IFE包装函数，改用createCommonjsModule函数进行模块化处理，
    优化代码布局和格式，提升代码可读性和维护性。

--- gulpfile.js
+++ gulpfile.js
@@ -7,134 +7,123 @@ var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
 var header = require("gulp-header");
 
-var rollup = require("rollup");
+var rollupO = require("rollup");
 var commonjs = require("rollup-plugin-commonjs");
 var resolve = require("rollup-plugin-node-resolve");
-
 
 // var istanbul = require("gulp-istanbul");
 var package = require("./package.json");
 var banner =
-	'/*!\n' +
-	' * ' + package.name + ' v' + package.version + '\n' +
-	' * Homepage ' + package.homepage + '\n' +
-	' * License ' + package.license + '\n' +
-	' */\n'
-var outputDir = 'dist/'
-
-gulp.task('build', ['test-server','rollup','min'], function () {
-	
-	// return gulp.src('./browser-source/*.js')
-	// 	.pipe(browserify())
-	// 	.pipe(header(banner))
-	// 	.pipe(gulp.dest('./dist'))
-	// 	.pipe(uglify())
-	// 	.pipe(header(banner))
-	// 	.pipe(rename({
-	// 		suffix:".min"
-	// 	}))
-	// 	.pipe(gulp.dest('./dist'));
-
-	// return rollup.rollup({
-	// 	input: './nzh.js',
-	// 	plugins: [
-	// 		resolve(),
-	// 		commonjs()
-	// 	]
-	// }).then(function (bundle) {
-	// 	return bundle.write({
-	// 		file: './dist1/nzh.js',
-	// 		format: 'umd',
-	// 		name: 'Nzh',
-	// 		banner: banner,
-	// 		sourcemap: false
-	// 	})
-	// })
-})
+  "/*!\n" +
+  " * " +
+  package.name +
+  " v" +
+  package.version +
+  "\n" +
+  " * Homepage " +
+  package.homepage +
+  "\n" +
+  " * License " +
+  package.license +
+  "\n" +
+  " */\n";
+var outputDir = "dist/";
 
 function runRollup(input, output) {
-	var inputopt = {
-		// input: './nzh.js',
-		plugins: [
-			resolve(),
-			commonjs()
-		]
-	}
-	var outputopt = {
-		// file: './dist/nzh.js',
-		format: 'umd',
-		name: 'Nzh',
-		banner: banner,
-		sourcemap: false
-	}
-	return rollup.rollup(Object.assign({},inputopt,{
-		input: input
-	})).then(function (bundle) {
-		return bundle.write(Object.assign({},outputopt,{
-			file: output,
-		}))
-	})
+  var inputopt = {
+    // input: './nzh.js',
+    plugins: [resolve(), commonjs()],
+  };
+  var outputopt = {
+    // file: './dist/nzh.js',
+    format: "esm",
+    name: "Nzh",
+    banner: banner,
+    sourcemap: false,
+  };
+  return Promise.all([rollupO
+    .rollup(
+      Object.assign({}, inputopt, {
+        input: input,
+      })
+    )
+    .then(function (bundle) {
+      return bundle.write(
+        Object.assign({}, outputopt, {
+          file: output,
+        })
+      );
+    }), rollupO
+    .rollup(
+      Object.assign({}, inputopt, {
+        input: input,
+      })
+    )
+    .then(function (bundle) {
+      return bundle.write(
+        Object.assign({}, outputopt, {
+          file: output.replace('.js', '.esm.js'),
+					format: 'esm'
+        })
+      );
+    })]) ;
 }
 
-gulp.task('rollup', function () {
-	return Promise.all([
-		runRollup('./nzh.js',outputDir + 'nzh.js'),
-		runRollup('./cn.js',outputDir + 'nzh.cn.js'),
-		runRollup('./hk.js',outputDir + 'nzh.hk.js'),
-	])
-})
+function rollup() {
+  return Promise.all([
+    runRollup("./nzh.js", outputDir + "nzh.js"),
+    runRollup("./cn.js", outputDir + "nzh.cn.js"),
+    runRollup("./hk.js", outputDir + "nzh.hk.js"),
+  ]);
+}
 
-gulp.task('min',['rollup'],function () {
-	return gulp.src(['dist/!(*.min).js'])
-	.pipe(uglify())
-	.pipe(header(banner))
-	.pipe(rename({
-		suffix:".min"
-	}))
-	.pipe(gulp.dest(outputDir));
-})
-
-
-// gulp.task('pre-test', function () {
-//   return gulp.src(['src/**/*.js'])
-//     // Covering files 
-//     .pipe(istanbul())
-//     // Force `require` to return covered files 
-//     .pipe(istanbul.hookRequire());
-// });
-
-// gulp.task('test', ['pre-test'], function () {
-//   return gulp.src('test/mocha_*.js', {read: false})
-//     .pipe(mocha({reporter: 'dot'}))
-//     // Creating the reports after tests ran 
-//     .pipe(istanbul.writeReports())
-//     // Enforce a coverage of at least 90% 
-//     .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
-// });
-
-gulp.task('test', ['test-server', 'test-browser'])
-
-gulp.task('test-server', function () {
-	return gulp.src('test/test_mocha.js', { read: false })
-		.pipe(mocha({ reporter: 'dot' }))
+var min = gulp.series(rollup, function () {
+  return gulp
+    .src(["dist/!(*.min).js"])
+    .pipe(uglify())
+    .pipe(header(banner))
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(gulp.dest(outputDir));
 });
 
-gulp.task('output-testjs-browser', ['test-server'], function () {
-	return gulp.src('test/test_mocha.js')
-		.pipe(browserify({ insertGlobals: true }))
-		.pipe(rename({
-			basename: "tests"
-		}))
-		.pipe(gulp.dest('test/browser'));
-})
+function testServer() {
+  return gulp
+    .src("test/test_mocha.js", { read: false })
+    .pipe(mocha({ reporter: "dot" }));
+}
 
-gulp.task('test-browser', ['output-testjs-browser', 'build'], function () {
-	return gulp.src('test/browser/test.html')
-		.pipe(mochaPhantomjs({ reporter: 'dot' }))
-})
+function outputTestjsBrowser() {
+  return gulp.series(testServer, function () {
+    return gulp
+      .src("test/test_mocha.js")
+      .pipe(browserify({ insertGlobals: true }))
+      .pipe(
+        rename({
+          basename: "tests",
+        })
+      )
+      .pipe(gulp.dest("test/browser"));
+  });
+}
 
-gulp.task('test-docs', ['build'], function () {
+function testBrowser() {
+  return gulp.series(gulp.parallel(outputTestjsBrowser, build), function () {
+    return gulp
+      .src("test/browser/test.html")
+      .pipe(mochaPhantomjs({ reporter: "dot" }));
+  });
+}
 
-})
+function test() {
+  return gulp.parallel(testServer, testBrowser);
+}
 
-gulp.task('default', ['build', 'test']);
+var build = gulp.parallel(testServer, rollup, min);
+
+exports.build = build;
+
+exports.default = gulp.parallel(build, test);
